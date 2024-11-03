d44aaa8664559b0230f5fb0a14b522753a988fd2
Heylium <tuanhsiang@163.com>
2024-11-04 00:58:52 +0800

    redis operations controller added.

--- gateway-1000/src/main/java/org/wechat/controller/HelloController.java
+++ gateway-1000/src/main/java/org/wechat/controller/HelloController.java
@@ -1,15 +1,32 @@
 package org.wechat.controller;
 
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.wechat.base.BaseInfoProperties;
+import org.wechat.utils.RedisOperator;
 
 @RestController
-@RequestMapping("gateway")
-public class HelloController {
+@RequestMapping("g")
+public class HelloController extends BaseInfoProperties {
+
+//    @Resource
+//    private RedisOperator redisOperator;
 
     @GetMapping("hello")
     public Object hello() {
         return "Hello World from gateway";
+    }
+
+    @GetMapping("setRedis")
+    public Object setRedis(String k, String v) {
+        redis.set(k, v);
+        return "setRedis Ok~";
+    }
+
+    @GetMapping("getRedis")
+    public Object getRedis(String k) {
+        return redis.get(k);
     }
 }
